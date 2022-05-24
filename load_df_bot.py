8382db79930953c567e7ca0f24aaf597051cb8d7
Jorgvt <vila_tronik@hotmail.com>
2022-05-24 19:50:06 +0200

    :sparkles: Añadida la posibilidad de entrenar un modelo de clasificación (Regresión Logística)

--- load_df_bot.py
+++ load_df_bot.py
@@ -1,4 +1,6 @@
 import requests
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import telebot
@@ -23,7 +25,7 @@ def download_df(message):
     bot.reply_to(message, 'El DataFrame tiene {} filas y {} columnas.'.format(*df.shape))
 
 @bot.message_handler(commands=['histograma'])
-def plot_histogram(message):
+def start_histogram(message):
     if df is None:
         bot.reply_to(message, 'Primero tienes que subir un archivos de datos.')
         return
@@ -45,6 +47,40 @@ def plot_histogram(message):
     bot.set_state(user_id=message.from_user.id, 
                   state=None, 
                   chat_id=message.chat.id)
+
+@bot.message_handler(commands=['clasificacion'])
+def start_classification_model(message):
+    if df is None:
+        bot.reply_to(message, 'Primero tienes que subir un archivos de datos.')
+        return
+    bot.set_state(user_id=message.from_user.id, 
+                  state='clasificacion', 
+                  chat_id=message.chat.id)
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, 
+                                               selective=False)
+    items = [telebot.types.KeyboardButton(var) for var in df.columns]
+    markup.add(*items)
+    bot.send_message(message.chat.id, 
+                     'Indica qué variable quieres predecir.\n'+ 
+                     'En la versión de prueba solamente está permitido predecir una variable.', reply_markup=markup)
+
+@bot.message_handler(func=lambda m: m.text in df.columns and bot.get_state(m.from_user.id, m.chat.id)=='clasificacion')
+def train_classification_model(message):
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import cross_val_score
+    
+    df_temp = df.copy()
+    Y_train = df_temp.pop(message.text)
+    X_train = df_temp.copy()
+    results = cross_val_score(LogisticRegression(), X_train, Y_train, cv=5)
+
+    bot.reply_to(message, f'La regresión logística obtiene una precisión de {results.mean():.2f} +- {results.std():.2f}.\n'+
+                           'Recuerda que si quieres poder guardar el modelo o ajustar los parámetros necesitas una suscripción de pago.')
+    
+    bot.set_state(user_id=message.from_user.id, 
+                  state=None, 
+                  chat_id=message.chat.id)
+
 
 ## Starting the bot
 bot.infinity_polling()
